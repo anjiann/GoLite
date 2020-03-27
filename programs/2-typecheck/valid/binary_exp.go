a3d25b0f4f8a55258548b14aabeb4b0ef11c3983
ajian4 <jia.jian@mail.mcgill.ca>
2020-03-27 19:02:46 -0400

    added milestone report doc

---
+++ programs/2-typecheck/valid/binary_exp.go
@@ -0,0 +1,41 @@
+package main
+
+func main () {
+	var integer int = 0
+	var float float64 = 3.14
+	var char rune = 'c'
+	var boolean bool = false
+	var str string = "hello world"
+
+	//binary
+	var orExp = boolean || boolean
+	var andExp = boolean && boolean
+
+	var equalExp = integer == integer
+	equalExp = float == float
+	equalExp = char == char
+	equalExp = boolean == boolean
+	equalExp = str == str
+
+	var notequalExp = integer != integer
+	notequalExp = float != float
+	notequalExp = char != char
+	notequalExp = boolean != boolean
+	notequalExp = str != str
+
+	lessExp,//       <
+	lesseqExp, //    <= 
+	greaterExp, //   >
+	greatereqExp, // >=
+	plusExp, //      +
+	minusExp, //     -
+	multExp, //   	 *
+	divExp, //    	 /
+	modExp, //       %
+	bitOrExp, //     |
+	bitAndExp, //    &
+	lshiftExp, //    <<
+	rshiftExp, //    >>
+	andnotExp, //    &^
+	xorExp, //       ^
+}
